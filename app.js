fe4fc8fa9f45d92fbfe6eba90863893703d2ab04
saintthor <saintthor@gmail.com>
2022-08-26 16:23:09 +0800

    set play.html to default.

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var options = {
   dotfiles: 'ignore',
   etag: false,
   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
+  index: ['play.html'],
+//   index: ['index.html'],
   maxAge: '1m',
   redirect: false
 }
